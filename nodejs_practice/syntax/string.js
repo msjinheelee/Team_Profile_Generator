b6466a4d56397c0802e03aeaba31878861f3273d
JINHEE LEE <ms.jinhee.lee@gmail.com>
2022-05-06 12:48:04 +1000

    practice test

---
+++ nodejs_practice/syntax/string.js
@@ -0,0 +1,6 @@
+console.log('1');
+console.log('1+1');
+console.log('1'+'1');
+
+console.log('안녕 나는 이진희야'.length);
+console.log('hello im jinhee'.length);
---
+++ nodejs_practice/syntax/variable.js
@@ -0,0 +1,11 @@
+a = 1; 
+// 대입연산자
+console.log(a);
+a = 2; 
+console.log(a);
+
+var name = "jin"
+var letter = "skdfkslkdfsdfsfsd "+ name +" sf sdf a s ada drklsrts tgegegfd a"+ name +"\n\ asd ad ad asfafa fdsfsd"+ name +" sldifwekfjsl"+ name +"sldfksjlfks"+ name +"fjlsf";
+
+console.log(letter);
+// \n\ 줄바꿈
